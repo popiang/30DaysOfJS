4cea7b75545a0ad63bfaca7f518a66f034e542a2
SHAHRIL MAD SHAH <popiang@hotmail.com>
2025-02-12 20:55:06 +0800

    asdf

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+// const name = "Shahril";
+// // const age = 46;
+// const isHandsome = true;
+// const ghost = undefined;
+// const disappear = null;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isHandsome);
+// console.log(typeof ghost);
+// console.log(typeof disappear);
+
+// let city;
+// let state;
+// // let country;
+// let language;
+
+// const dogName = "John";
+// const dogAge = 8;
+// const isDogMale = true;
+// const dogBreed = undefined;
+
+// // const firstName = "Shahril";
+// // const lastName = "Mad Shah";
+// // const maritalStatus = "Single";
+// // const country = "Malaysia";
+// // const age = 46;
+
+// const firstName = "Shahril"; lastName = "Mad Shah"; maritalStatus = "Single"; country = "Malaysia"; age = 46;
+
+// console.log(firstName, lastName, maritalStatus, country, age);
+
+const myAge = 25;
+const yourAge = 30;
+
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old`);
---
+++ variable.js
@@ -0,0 +1,6 @@
+const name = "Shahril";
+const isHandsome = true;
+const ghost = undefined;
+const disappear = null;
+
+console.log(name, isHandsome, ghost, disappear)
